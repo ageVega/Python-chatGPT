7d04b8d76a2d4380d29b698fd1f385f41a1851d0
ageVega <agevega@hotmail.com>
2023-03-15 20:26:05 +0100

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -1,16 +1,29 @@
 import openai 
+from dotenv import load_dotenv
+from os import environ
 
-openai.api_key = ""
+load_dotenv()
+openai.api_key = environ.get('OPENAI_API_KEY')
+
+Engine = "text-davinci-003"
+Max_tokens = 4000
 
 while True:
 
     prompt = input("\n- Introduce una pregunta:\n")
+    
+    while prompt == "":
+        print("No has introducido ninguna pregunta. Por favor, inténtalo de nuevo.")
+        prompt = input("\n- Introduce una pregunta:\n")
 
     if prompt == "exit":
-        break
+        break    
 
-    completion = openai.Completion.create(engine="gpt-3.5-turbo",
-                                        prompt=prompt,
-                                        max_tokens=4000)
 
+    completion = openai.Completion.create(
+        engine=Engine,
+        prompt=prompt,
+        max_tokens=Max_tokens)
+
+ 
     print(completion.choices[0].text)
